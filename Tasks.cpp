15229deb88184902cd9752271f5b3b475e5e96c9
Chris <nastoski@msu.edu>
2022-08-09 18:22:31 -0400

    changed tasks.cpp to add a task only when there is text in the field

--- Tasks.cpp
+++ Tasks.cpp
@@ -12,8 +12,11 @@ Tasks::Tasks(QWidget * tasks_page_)
 // add a task to the vector
 void Tasks::addTask() {
     Task task = Task(textInputWidget->text(), QString()); // add description handling soon!
-    tasks.push_back(task);
-    textInputWidget->clear();
+    if(textInputWidget->text() != ""){
+        tasks.push_back(task);
+        textInputWidget->clear();
+    }
+
 }
 
 // return list of tasks
@@ -21,7 +24,7 @@ void Tasks::displayTasks() {
     QString text;
 
        // consider fixing this compiler warning, though harmless for now
-    for (int i = 0; i < tasks.size(); i++) {
+    for (auto i = 0; i < tasks.size(); i++) {
         QString index = QString::fromStdString(std::to_string(i + 1)); // :)
         text += index + ". " + tasks[i].title + "\n";
     }
